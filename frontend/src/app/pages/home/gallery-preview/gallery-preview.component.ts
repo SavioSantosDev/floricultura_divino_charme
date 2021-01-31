20171240f853be9f4a7c4c872895160f92437016
saviogta1 <saviogta1@gmail.com>
2021-01-31 20:09:35 -0300

    home-page

--- frontend/src/app/pages/home/gallery-preview/gallery-preview.component.ts
+++ frontend/src/app/pages/home/gallery-preview/gallery-preview.component.ts
@@ -3,9 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gallery-preview',
   templateUrl: './gallery-preview.component.html',
-  styleUrls: ['./gallery-preview.component.scss']
+  styleUrls: ['./gallery-preview.component.scss', '../../gallery.scss']
 })
 export class GalleryPreviewComponent implements OnInit {
+
+  // Cada array interno é uma linha
+  imgPathBase = '../../../../assets/images/';
+  gallery = [
+    [ `${this.imgPathBase}img5.jpg`, `${this.imgPathBase}img6.jpg`, `${this.imgPathBase}img7.jpg` ],
+    [ `${this.imgPathBase}img8.jpg`, `${this.imgPathBase}img9.jpg`, `${this.imgPathBase}img4.jpg` ],
+  ]
 
   constructor() { }
 
